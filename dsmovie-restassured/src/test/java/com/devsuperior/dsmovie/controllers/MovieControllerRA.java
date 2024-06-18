f56e7fb5fb595c81d5206715ac039f5fb043fcd7
ElderBR <elderbarbosaromao@gmail.com>
2024-06-18 12:39:58 -0300

    Criado o teste findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty

--- dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,49 +1,73 @@
 package com.devsuperior.dsmovie.controllers;
 
+import io.restassured.http.ContentType;
 import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class MovieControllerRA {
 
-	@BeforeEach
-	void setUp() throws Exception {
-		baseURI = "http://localhost:8080";
-	}
-	
-	@Test
-	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() throws Exception {
-		given()
-				.get("/movies")
-				.then()
-				.statusCode(200)
-		;
-	}
-	
-	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty(){
-	}
-	
-	@Test
-	public void findByIdShouldReturnMovieWhenIdExists(){
-	}
-	
-	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
-	}
-	
-	@Test
-	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {		
-	}
-	
-	@Test
-	public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
-	}
-	
-	@Test
-	public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
-	}
+    private Integer exitingMovieId;
+    private String exitingMovieTitle;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        baseURI = "http://localhost:8080";
+
+        exitingMovieId = 1;
+        exitingMovieTitle = "The Witcher";
+    }
+
+    @Test
+    public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() throws Exception {
+        given()
+                .get("/movies")
+                .then()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+        given()
+                .header("Content-Type", "application-json")
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .param("title", exitingMovieTitle)
+                .when()
+                .get("/movies")
+                .then()
+                .statusCode(200)
+                .body("content.id[0]", is(1))
+                .body("content.title[0]", equalTo(exitingMovieTitle))
+                .body("content.score[0]", is(4.5F))
+                .body("content.count[0]", is(2))
+                .body("content.image[0]", equalTo("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"))
+        ;
+    }
+
+    @Test
+    public void findByIdShouldReturnMovieWhenIdExists() {
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+    }
+
+    @Test
+    public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() throws JSONException {
+    }
+
+    @Test
+    public void insertShouldReturnForbiddenWhenClientLogged() throws Exception {
+    }
+
+    @Test
+    public void insertShouldReturnUnauthorizedWhenInvalidToken() throws Exception {
+    }
 }
